bd7dbcbfb411b14f2d51a96858f714410f6f21e5
wasy38 <alex5perevalov@mail.ru>
2021-10-15 20:09:35 +0700

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void ButtonClick(View view) {
+        TextView textView = (TextView)findViewById(R.id.textView);
+        textView.setText("LET MY DIE");
+    }
+}
